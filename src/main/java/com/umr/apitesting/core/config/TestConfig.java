5ab4ee224db56ebf480a1eb2cbac6700ce395b93
zahidanwarusa <zahidanwarusa@gmail.com>
2024-12-30 14:02:08 -0500

    no as such update

--- src/main/java/com/umr/apitesting/core/config/TestConfig.java
+++ src/main/java/com/umr/apitesting/core/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.umr.apitesting.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,16 +8,25 @@ import com.umr.apitesting.core.executor.ParallelTestExecutor;
 
 @Configuration
 public class TestConfig {
+
+	@Value("${test.suite.path}")
+	private String testSuitePath;
+
+	@Bean
+	public String testDataPath() {
+		return testSuitePath;
+	}
+
 	private final ConfigManager configManager;
 
 	public TestConfig(ConfigManager configManager) { // Constructor injection
 		this.configManager = configManager;
 	}
 
-	@Bean
-	public String testDataPath() {
-		return configManager.getProperty("test.suite.path");
-	}
+//	@Bean
+//	public String testDataPath() {
+//		return configManager.getProperty("test.suite.path");
+//	}
 
 	@Bean
 	public Integer threadCount() {
